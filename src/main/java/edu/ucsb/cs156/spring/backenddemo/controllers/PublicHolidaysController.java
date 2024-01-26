40703982d9a233526a231ae9e3666f5f6d8484e5
Daoyijiucsb <daoyi@umail.ucsb.edu>
2024-01-26 00:45:05 -0800

    daoyi -fix typo

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/PublicHolidaysController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/PublicHolidaysController.java
@@ -28,12 +28,11 @@ public class PublicHolidaysController {
 
     @Autowired
     PublicHolidayQueryService publicHolidayQueryService;
-
     @Operation(summary = "Get list of public holidays from year and country code", description = "JSON return format documented here: https://date.nager.at/Api")
     @GetMapping("/get")
     public ResponseEntity<String> getPublicHoliday(
-        @Parameter(name="year", description="number fo years", example="2023") @RequestParam String year,
-        @Parameter(name="countryCode", description="code of country", example="us") @RequestParam String countryCode
+        @Parameter(name="year", description="year", example="2023") @RequestParam String year,
+        @Parameter(name="countryCode", description="country code", example="us") @RequestParam String countryCode
     ) throws JsonProcessingException {
         log.info("getPublicHoliday: year={} countryCode={}", year, countryCode);
         String result = publicHolidayQueryService.getJSON(year, countryCode);
